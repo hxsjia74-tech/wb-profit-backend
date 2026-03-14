278688d200eba535a0e9fcbdbc046c6ec17de58d
hxsjia74-tech <hxsjia74@gmail.com>
2026-03-14 17:46:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const { Pool } = require("pg");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
