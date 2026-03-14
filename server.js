a945e14d23df56a6ecd33a88faaad9767e9b4581
hxsjia74-tech <hxsjia74@gmail.com>
2026-03-15 00:46:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -143,11 +143,6 @@ app.get("/profit", async (req, res) => {
     });
   }
 });
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 app.get("/profit-by-article", async (req, res) => {
   try {
 
